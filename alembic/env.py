f3f72537c51d5f127fa87a0e47901635e7cbcb48
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-12-24 04:52:34 +0100

    cors fix:

--- alembic/env.py
+++ alembic/env.py
@@ -7,12 +7,13 @@ from alembic import context
 from api.models import Base
 
 from api.config import settings
+import urllib.parse
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option(
-    "sqlalchemy.url", f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}")
+    "sqlalchemy.url", f"postgresql://{settings.database_username}:{urllib.parse.quote(settings.database_password)}@{settings.database_hostname}/{settings.database_name}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- api/main.py
+++ api/main.py
@@ -9,7 +9,7 @@ from starlette.middleware.sessions import SessionMiddleware
 origins = [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://helpmeout-bice.vercel.app/",
+    "https://helpmeout-bice.vercel.app",
     "http://127.0.0.1/5173",
     "http://127.0.0.1/3000"
 ]
